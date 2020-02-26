3bab04733bc1de8301d36a08299d94345fbbe971
Júlio César Bueno <buenojc@outlook.com>
2020-02-26 17:19:37 -0300

    Server can recive data from form (WIP)
    and push to an array

--- server.js
+++ server.js
@@ -4,14 +4,49 @@ const server = express();
 
 server.set("view engine", "njk");
 server.use(express.static("./public"));
+server.use(express.urlencoded({ extended: true }));
 
 nunjucks.configure("./views", {
   express: server,
+  autoescape: false,
   noCache: true
 });
 
+const donors = [
+  {
+    name: "Julio Cesar",
+    email: "buenojc@outlook.com",
+    blood: "A+"
+  },
+  {
+    name: "Julio Cesar",
+    email: "buenojc@outlook.com",
+    blood: "A+"
+  },
+  {
+    name: "Julio Cesar",
+    email: "buenojc@outlook.com",
+    blood: "A+"
+  },
+  {
+    name: "Julio Cesar",
+    email: "buenojc@outlook.com",
+    blood: "A+"
+  }
+];
+
 server.get("/", function(req, res) {
-  res.render("index");
+  return res.render("index", { donors });
+});
+
+server.post("/", function(req, res) {
+  const donor = {
+    name: req.body.name,
+    email: req.body.email,
+    blood: req.body.blood
+  };
+  donors.push(donor);
+  return res.redirect("/");
 });
 
 server.listen(5000, function() {
